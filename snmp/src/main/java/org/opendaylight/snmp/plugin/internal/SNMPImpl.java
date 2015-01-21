e97c97de79eb39a24f09b7fce3c940f61b52e56f
Steve <sdean@hp.com>
2015-01-21 15:02:49 -0800

    1/22/2015: I made the changes that Nathan found in patch sets 4, 5 and 6. These changes are all in patch set 7.
    fixed bug in SNMPImpl, created karaf feature, modified pom files to build SNAPSHOT version
    
    Change-Id: Ic4a213a10db2a2e082609aabf42f874faf45d420
    Signed-off-by: Steve <sdean@hp.com>

--- snmp/src/main/java/org/opendaylight/snmp/plugin/internal/SNMPImpl.java
+++ snmp/src/main/java/org/opendaylight/snmp/plugin/internal/SNMPImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2014-2015 Cisco Systems, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -121,7 +121,7 @@ public class SNMPImpl implements SnmpService{
                         if (binding.getOid() == null ||
                                 binding.getOid().size() < oid.size() ||
                                 oid.leftMostCompare(oid.size(), binding.getOid()) != 0 ||
-                                binding.getOid().compareTo(oid) <= 0) {
+                                binding.getOid().compareTo(oid) < 0) {
 
                             stop = true;
                             break;
