98a2ae9a546d8168326e5362d9d78b0f11786e2c
Steve Dean <sdean@hp.com>
2015-01-26 13:11:00 -0800

    use Config Subsystem instead of Bundle Actavitor to start SNMP Plugin and get references to MD-SAL services
    Change-Id: I26d47c6ae1089b5c12e2e7467e33807b04abf0af
    Signed-off-by: Steve Dean <sdean@hp.com>

---
+++ snmp/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/snmp/rev140922/SNMPImplModule.java
@@ -0,0 +1,29 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.snmp.rev140922;
+
+import org.opendaylight.snmp.plugin.internal.SNMPImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SNMPImplModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.snmp.rev140922.AbstractSNMPImplModule {
+	private static final Logger LOG = LoggerFactory.getLogger(SNMPImplModule.class);
+	
+	public SNMPImplModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public SNMPImplModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.snmp.rev140922.SNMPImplModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        LOG.info("Creating SNMPImpl instance");
+        return new SNMPImpl(getRpcRegistryDependency());
+    }
+
+}
